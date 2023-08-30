be88baaffcccf9436ea369c6265f8f9c2da5f76a
Amadeo Marli <132928847+asc33nzio@users.noreply.github.com>
2023-08-30 20:55:01 +0700

    Various server initializations, added implicit folders, edited main config to accommodate dotenv, database configuration.
    - Added implicit folders
    - Edited main config to accommodate dotenv
    - Database configuration

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -2,29 +2,35 @@ require("dotenv/config");
 const express = require("express");
 const cors = require("cors");
 const { join } = require("path");
-
 const PORT = process.env.PORT || 8000;
-const app = express();
-app.use(
-  cors({
-    origin: [
-      process.env.WHITELISTED_DOMAIN &&
-        process.env.WHITELISTED_DOMAIN.split(","),
-    ],
-  })
-);
+const server = express();
+const db = require('./models');
+const { authRouters, productRouters } = require("./routers");
 
-app.use(express.json());
+// server.use(
+//   cors({
+//     origin: [
+//       process.env.WHITELISTED_DOMAIN &&
+//       process.env.WHITELISTED_DOMAIN.split(","),
+//     ],
+//   })
+// );
+
+server.use(cors());
+server.use(express.json());
+server.use(express.static('./public'));
 
 //#region API ROUTES
 // ===========================
 // NOTE : Add your routes here
+server.use('/api/products', productRouters);
+server.use('/api/users', authRouters);
 
-app.get("/api", (req, res) => {
-  res.send(`Hello, this is my API`);
+server.get("/api", (req, res) => {
+  res.send(`Hello, welcome to Alpha Mart API.`);
 });
 
-app.get("/api/greetings", (req, res, next) => {
+server.get("/api/greetings", (req, res, next) => {
   res.status(200).json({
     message: "Hello, Student !",
   });
@@ -33,7 +39,7 @@ app.get("/api/greetings", (req, res, next) => {
 // ===========================
 
 // not found
-app.use((req, res, next) => {
+server.use((req, res, next) => {
   if (req.path.includes("/api/")) {
     res.status(404).send("Not found !");
   } else {
@@ -42,7 +48,7 @@ app.use((req, res, next) => {
 });
 
 // error
-app.use((err, req, res, next) => {
+server.use((err, req, res, next) => {
   if (req.path.includes("/api/")) {
     console.error("Error : ", err.stack);
     res.status(500).send("Error !");
@@ -55,19 +61,19 @@ app.use((err, req, res, next) => {
 
 //#region CLIENT
 const clientPath = "../../client/build";
-app.use(express.static(join(__dirname, clientPath)));
+server.use(express.static(join(__dirname, clientPath)));
 
 // Serve the HTML page
-app.get("*", (req, res) => {
+server.get("*", (req, res) => {
   res.sendFile(join(__dirname, clientPath, "index.html"));
 });
 
 //#endregion
-
-app.listen(PORT, (err) => {
+server.listen(PORT, (err) => {
   if (err) {
     console.log(`ERROR: ${err}`);
   } else {
-    console.log(`APP RUNNING at ${PORT} ✅`);
+    db.sequelize.sync({ alter: true });
+    console.log(`SERVER RUNNING at ${PORT} ✅`);
   }
 });
---
+++ projects/server/src/routers/productRouters.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const productControllers = require('../controllers/productControllers');
+
+
+module.exports = router;
