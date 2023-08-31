111fd1e647c6f7dc90b00749cfca363d63acadb5
alvianaryobimo <117703346+alvianaryobimo@users.noreply.github.com>
2023-08-31 22:11:50 +0700

    [OGWA-14] As a user, I want to input my username/email and password, so that I can login to the web app
    As a user, I want to input my username/email and password, so that I can login to the web app

--- projects/server/src/controllers/userControllers.js
+++ projects/server/src/controllers/userControllers.js
@@ -19,11 +19,44 @@ module.exports = {
                 }
             });
             if (!checkLogin) throw { message: "User not Found." };
+            if (checkLogin.RoleId != 1) throw { message: "You have to Login on admin login" }
 
             const isValid = await bcrypt.compare(password, checkLogin.password);
             if (!isValid) throw { message: "Password Incorrect." };
 
             const payload = { id: checkLogin.id };
+            const token = jwt.sign(payload, process.env.KEY_JWT, { expiresIn: "3h" });
+
+            res.status(200).send({
+                message: "Login success",
+                token
+            });
+        } catch (error) {
+            res.status(500).send({
+                error,
+                status: 500,
+                message: 'Internal server error',
+            });
+        }
+    },
+    adminLogin: async (req, res) => {
+        try {
+            const { data, password } = req.body;
+            const checkLogin = await user.findOne({
+                where: {
+                    [Op.or]: [
+                        { email: data },
+                        { username: data },
+                    ]
+                }
+            });
+            if (!checkLogin) throw { message: "User not Found." };
+            if (checkLogin.RoleId == 1) throw { message: "You have to Login on user Login." }
+
+            const isValid = await bcrypt.compare(password, checkLogin.password);
+            if (!isValid) throw { message: "Password Incorrect." };
+
+            const payload = { id: checkLogin.id, RoleId: checkLogin.RoleId };
             const token = jwt.sign(payload, process.env.KEY_JWT, { expiresIn: "3h" });
 
             res.status(200).send({
