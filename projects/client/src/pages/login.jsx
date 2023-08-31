0a1b187db88088283a8c93740adf992179a0b1e1
alvianaryobimo <117703346+alvianaryobimo@users.noreply.github.com>
2023-08-31 22:29:10 +0700

    [OGWA-14] As a user, I want to input my username/email and password, so that I can login to the web app (#14)
    * [OGWA-14] As a user, I want to input my username/email and password, so that I can login to the web app
    
    As a user, I want to input my username/email and password, so that I can login to the web app
    
    * Squashed commit of the following:
    
    commit 0ed37f3ae61e14c41ecca1b9d5565e11d7d28e0e
    Author: Amadeo Marli <132928847+asc33nzio@users.noreply.github.com>
    Date:   Thu Aug 31 10:26:40 2023 +0700
    
        Deleted Old Middleware Folder
    
    commit b3d694586fb384fd13ad38276cd923813590416d
    Author: Amadeo Marli <132928847+asc33nzio@users.noreply.github.com>
    Date:   Thu Aug 31 10:06:01 2023 +0700
    
        Naming adjustments, controller index no longer destructured, added dotenv template, initialized folders
    
        - Naming adjustments
        - controller index no longer destructured
        - added dotenv template
        - initialized folders
    
    * As a user, I want to input my username/email and password, so that I can login to the web app
    
    As a user, I want to input my username/email and password, so that I can login to the web app
    
    * [OGWA-14] As a user, I want to input my username/email and password, so that I can login to the web app
    
    As a user, I want to input my username/email and password, so that I can login to the web app
    
    * [OGWA-14] As a user, I want to input my username/email and password, so that I can login to the web app
    
    As a user, I want to input my username/email and password, so that I can login to the web app
    
    * [OGWA-14] As a user, I want to input my username/email and password, so that I can login to the web app
    
    As a user, I want to input my username/email and password, so that I can login to the web app
    
    * [OGWA-14] As a user, I want to input my username/email and password, so that I can login to the web app
    
    As a user, I want to input my username/email and password, so that I can login to the web app

--- projects/client/src/pages/login.jsx
+++ projects/client/src/pages/login.jsx
@@ -4,11 +4,18 @@ import sourceLogoBlack from "../assets/public/AM_logo_trans.png"
 import sourceGraphic from "../assets/public/AM_graphic.png"
 import { Link } from "react-router-dom"
 import { UserLogin } from "../views/Login/components/userLogin";
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
-import { Tabs, TabPanels, TabPanel } from '@chakra-ui/react'
-// import { BsFillPersonFill, BsFillPersonPlusFill } from 'react-icons/bs';
+import { AdminLogin } from "../views/Login/components/adminLogin"
+import { Box, Flex, Image, TabIndicator, Text } from "@chakra-ui/react";
+import { Tab, Tabs, TabPanels, TabPanel, TabList } from '@chakra-ui/react'
+import { BsFillPersonFill, BsFillPersonPlusFill } from 'react-icons/bs';
 
 export const Login = () => {
+    // const token = localStorage.getItem("token");
+    // useEffect(() => {
+    //     // if (token) {
+    //     //     navigate("/");
+    //     // }
+    // }, []);
     return (
         <>
             <Image w={"full"} h={"100vh"} src={source} position={"absolute"} />
@@ -17,7 +24,8 @@ export const Login = () => {
                     w={["200px", "300px", "400px"]} h={["500px"]}
                     bg={"#373433"} marginY={"auto"} boxShadow='0px 0px 10px black'>
                     <Flex mt={"50px"} justifyContent={"center"}>
-                        <Image w={["100px", "200px", "300px"]} src={sourceLogo} />
+                        <Image w={["100px", "200px", "300px"]} src={sourceLogo}
+                            transition="transform 0.3s ease-in-out" _hover={{ transform: "scale(1.1)" }} />
                     </Flex>
                     <Flex mt={"56.7px"} justifyContent={"center"}>
                         <Image w={["100px", "200px", "400px"]} src={sourceGraphic} />
@@ -25,30 +33,35 @@ export const Login = () => {
                 </Box>
                 <Box w={["260px", "380px", "400px"]} h={"500px"}
                     bg={"#F6F6F6"} marginY={"auto"} boxShadow='0px 0px 10px black'>
-                    <Flex display={{ base: "flex", md: "none" }} mt={"25px"} justifyContent={"center"}>
+                    <Flex display={{ base: "flex", md: "none" }} mt={"15px"} justifyContent={"center"}>
                         <Image w={["200px"]} src={sourceLogoBlack} />
                     </Flex>
-                    <Box mt={["45px", "40px", "150px"]} >
-                        <Heading ml={"50px"} mt={"20px"}
-                            fontSize={"30px"} fontFamily={"monospace"}>Shop Now.</Heading>
+                    <Box>
                         <Tabs mt={"10px"} align="center" variant='unstyled'>
                             <TabPanels>
                                 <TabPanel >
                                     <UserLogin />
                                 </TabPanel>
                                 <TabPanel>
-                                    <UserLogin />
+                                    <AdminLogin />
                                 </TabPanel>
                             </TabPanels>
-                            {/* <TabList>
-                                <Tab><BsFillPersonFill mt={"2px"} size={30} /></Tab>
-                                <Tab><BsFillPersonPlusFill mt={"2px"} size={30} /></Tab>
-                            </TabList> */}
+                            <TabList mt={"15px"}>
+                                <Tab ><BsFillPersonFill mt={"2px"} size={[22]} />
+                                    <Text>‎ User
+                                    </Text>
+                                </Tab>
+                                <Tab><BsFillPersonPlusFill mt={"2px"} size={22} />
+                                    <Text>‎ Admin </Text>
+                                </Tab>
+                            </TabList>
+                            <TabIndicator mt="-1.5px" height="2px" bg="#373433" borderRadius="1px" />
                         </Tabs>
                     </Box>
-                    <Flex mt={["60px", "70px", "100px"]} fontSize={"12px"} justifyContent={"center"} >
+                    <Flex mt={["30px", "35px", "45px"]} fontSize={"12px"} justifyContent={"center"} >
                         <Text>Don't have an account? ‎</Text>
-                        <Text as={Link} to="/" color={"gray"}>Click Here</Text>
+                        <Text as={Link} to="/" color={"gray"}
+                            transition="transform 0.3s ease-in-out" _hover={{ transform: "scale(1.1)" }} >Click Here</Text>
                     </Flex>
                 </Box>
             </Flex>
