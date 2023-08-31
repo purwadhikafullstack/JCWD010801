2fa3a775e5c2ad0d7b475b3ee0dc74050fd9feda
rifkyrizkita <rifky.rizkita.g@gmail.com>
2023-08-31 21:15:40 +0700

    [OGWA-12] As a non-registered user, I want to input my data, so that I can register as a user
    As a non-registered user, I want to input my data, so that I can register as a user

--- projects/server/src/controllers/userControllers.js
+++ projects/server/src/controllers/userControllers.js
@@ -45,4 +45,45 @@ module.exports = {
             });
         }
     },
+    register: async (req, res) => {
+      try {
+        const { username, firstName, lastName,email, phone, password } = req.body;
+        const isAccountExist = await user.findOne({
+          where: { [Op.or]: { username, email } },
+        });
+        if (isAccountExist && isAccountExist.email === email) {
+          throw { message: "Enail has been used" };
+        } else if (isAccountExist && isAccountExist.username === username) {
+          throw { message: "Username has been used" };
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+        const result = await user.create({
+          username,
+          firstName,
+          lastName,
+          email,
+          phone,
+          password: hashPassword,
+        });
+        const payload = { id: result.id };
+        const token = jwt.sign(payload, process.env.KEY_JWT, { expiresIn: "1h" });
+        const data = await fs.readFileSync("./src/templates/templateVerification.html", "utf-8");
+        const tempCompile = await handlebars.compile(data);
+        const tempResult = tempCompile({ username, token });
+        await transporter.sendMail({
+          from: process.env.NODEMAILER_USER,
+          to: email,
+          subject: "Verify account",
+          html: tempResult,
+        });
+        res.status(200).send({
+          status: true,
+          message: "Register success. Check your email to verify",
+          result,
+        });
+      } catch (error) {
+        res.status(400).send(error);
+      }
+    },
 }
