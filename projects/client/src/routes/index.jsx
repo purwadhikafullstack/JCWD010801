cc607b2378e7367de053ea668e45fae7b571301b
rifkyrizkita <rifky.rizkita.g@gmail.com>
2023-09-01 11:08:22 +0700

    [OGWA-12] As a non-registered user, I want to input my data, so that I can register as a user
    As a non-registered user, I want to input my data, so that I can register as a user

--- projects/client/src/routes/index.jsx
+++ projects/client/src/routes/index.jsx
@@ -2,7 +2,7 @@ import { Route, createRoutesFromElements } from "react-router-dom";
 
 import Homepage from "../pages/Home";
 import { Login } from "../pages/login";
-import { Registerpage } from "../pages/register1";
+import { Registerpage } from "../pages/register";
 
 const Routes = (
   <>
