f98d9528b9ad669c8ff2a38a10b97dc4e2c5a203
alvianaryobimo <117703346+alvianaryobimo@users.noreply.github.com>
2023-09-04 11:04:57 +0700

    [OGWA-34] As a super admin, I can create admin branch through user management settings page
    As a super admin, I can create admin branch through user management settings page

---
+++ projects/client/src/pages/brancheslist.jsx
@@ -0,0 +1,9 @@
+
+
+export const BranchesList = () => {
+    return (
+        <>
+        cek
+        </>
+    )
+}
--- projects/client/src/routes/index.jsx
+++ projects/client/src/routes/index.jsx
@@ -3,12 +3,16 @@ import { Route, createRoutesFromElements } from "react-router-dom";
 import Homepage from "../pages/Home";
 import { Login } from "../pages/login";
 import { Registerpage } from "../pages/register";
+import { BranchesList } from "../pages/brancheslist";
+import { AdminsList } from "../pages/adminslist";
 
 const Routes = (
   <>
     <Route path="/" element={<Homepage />} />
     <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Registerpage />} />;
+    <Route path="/register" element={<Registerpage />} />
+    <Route path="/brancheslist" element={<BranchesList />} />
+    <Route path="/adminslist" element={<AdminsList />} />
   </>
 );
 
