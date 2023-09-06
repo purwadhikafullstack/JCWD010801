458ba96c439b4e50f9d416f75230c8ef837b9729
alvianaryobimo <117703346+alvianaryobimo@users.noreply.github.com>
2023-09-06 12:37:31 +0700

    [OGWA-34]As a super admin, I can create admin branch through user management settings page

--- projects/client/src/pages/adminslist.jsx
+++
@@ -1,7 +0,0 @@
-import { AdminListPage } from "../views/AdminList"
-
-const AdminsList = () => {
-    return <AdminListPage />
-}
-
-export default AdminsList;
--- projects/server/src/routers/adminRouters.js
+++ projects/server/src/routers/adminRouters.js
@@ -3,7 +3,7 @@ const { adminControllers } = require("../controllers");
 const { verifyToken } = require("../middlewares/auth");
 
 router.post("/adminregister", adminControllers.adminRegister);
-router.get("/allAdmins", adminControllers.getAllAdmins);
+router.get("/alladmins", adminControllers.getAllAdmins);
 router.get("/getbranches", adminControllers.getBranches);
 router.get("/getadmin/:id", adminControllers.getAdmin);
 
