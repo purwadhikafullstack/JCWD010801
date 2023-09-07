24a694d298da3d60f99c5f45b6ef5c0652b3ed64
rifkyrizkita <rifky.rizkita.g@gmail.com>
2023-09-07 18:40:57 +0700

    [OGWA-15,OGWA-19&OGWA-20]
    As a user, I want to be able to change my password,
    As a user, I want to update my personal data such as name, gender, email and birthdate,
    As a user, I want to update my profile picture

--- projects/server/src/routers/userRouters.js
+++ projects/server/src/routers/userRouters.js
@@ -1,7 +1,8 @@
-const router = require('express').Router();
-const { userControllers } = require('../controllers');
+const router = require("express").Router();
+const { userControllers } = require("../controllers");
 const { verifyToken } = require("../middlewares/auth");
-const { checkRegister, checkEmail, checkResetPassword } = require("../middlewares/validator");
+const { checkRegister, checkEmail, checkResetPassword, checkChangePassword, checkUpdateEmail, checkUpdatePhone } = require("../middlewares/validator");
+const { multerUpload } = require("../middlewares/multer");
 
 router.post("/userlogin", userControllers.userLogin);
 router.post("/adminlogin", userControllers.adminLogin);
@@ -10,5 +11,10 @@ router.post("/register", checkRegister, userControllers.register);
 router.patch("/verification", verifyToken, userControllers.verificationAccount);
 router.put("/", checkEmail, userControllers.forgotPassword);
 router.patch("/reset", verifyToken, checkResetPassword, userControllers.resetPassword);
+router.patch("/profile", verifyToken, userControllers.updateProfile);
+router.patch("/email", checkUpdateEmail, verifyToken, userControllers.updateEmail);
+router.patch("/phone", checkUpdatePhone, verifyToken, userControllers.updatePhone);
+router.patch("/password", verifyToken, checkChangePassword, userControllers.changePassword);
+router.post("/avatar", verifyToken, multerUpload(`./src/public/avatars`, 'VA-IMG').single("file"), userControllers.updateAvatar);
 
 module.exports = router;
