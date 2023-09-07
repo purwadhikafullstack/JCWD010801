50f993b6a47182e9ac9033379e45bb83750fbe7c
alvianaryobimo <117703346+alvianaryobimo@users.noreply.github.com>
2023-09-07 15:11:11 +0700

    [OGWA-35] As an admin, I can see dashboard page
    As an admin, I can see dashboard page

--- projects/client/src/components/navigation/adminSidebar.jsx
+++ projects/client/src/components/navigation/adminSidebar.jsx
@@ -1,53 +1,106 @@
 import { BsGraphUpArrow, BsPersonGear } from "react-icons/bs";
-import sourceLogo from "../../assets/public/AM_logo_only_trans.png";
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { SlLogout } from "react-icons/sl";
+import { FaBars } from "react-icons/fa";
+import source from "../../assets/public/AM_logo_white.png";
+import sourceLogo from "../../assets/public/AM_logo_only_white_trans.png";
+import { Box, Flex, IconButton, Image, Text } from "@chakra-ui/react";
 import { AiOutlineBranches } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const AdminSidebar = () => {
+	const [navSize, setNavsize] = useState("small");
 	return (
 		<Flex
-			w={"100px"}
+			w={navSize === "small" ? "100px" : "200px"}
 			h={"100vh"}
-			bg={"gray.50"}
+			backgroundColor={"blackAlpha.900"}
 			borderTopRightRadius={"30px"}
 			boxShadow="0px 0px 8px black"
+			justifyContent={"space-between"}
 			direction={"column"}
 		>
-			<Flex as={Link} to={"/"} justifyContent={"center"}>
-				<Image
-					w={"90px"}
-					src={sourceLogo}
-					transition="transform 0.3s ease-in-out"
-					_hover={{ transform: "scale(1.1)" }}
-				/>
+			<Flex mt={"10px"} direction={"column"}>
+				<Flex justifyContent={"center"} as={Link} to={"/"}>
+					<Image
+						w={navSize === "small" ? "60px" : "120px"}
+						src={navSize === "small" ? sourceLogo : source}
+						transition="transform 0.3s ease-in-out"
+						_hover={{ transform: "scale(1.1)" }}
+					/>
+				</Flex>
+				<Flex justifyContent={"center"}>
+					<IconButton
+						left={"7px"}
+						variant={"unstyled"}
+						icon={<FaBars size={25} />}
+						color={"white"}
+						onClick={() => {
+							navSize === "small" ? setNavsize("large") : setNavsize("small");
+						}}
+					/>
+					{navSize === "large" ? (
+						<Text color={"white"} mt={"3px"} fontSize={"20px"}>
+							Menu
+						</Text>
+					) : null}
+				</Flex>
 			</Flex>
 			<Box>
 				<Flex
 					mb={"20px"}
+					color={"white"}
 					transition="transform 0.3s ease-in-out"
 					_hover={{ transform: "scale(1.2)" }}
-					justifyContent={"center"}
+					ml={navSize === "large" ? "20px" : "0px"}
+					justifyContent={navSize === "large" ? "start" : "center"}
 				>
 					<AiOutlineBranches size={30} />
+					{navSize === "large" ? (
+						<Text color={"white"} ml={"11px"} mt={"2px"} fontSize={"16px"}>
+							Branches
+						</Text>
+					) : null}
 				</Flex>
 				<Flex
 					mb={"20px"}
+					color={"white"}
 					transition="transform 0.3s ease-in-out"
 					_hover={{ transform: "scale(1.2)" }}
-					justifyContent={"center"}
+					ml={navSize === "large" ? "24px" : "0px"}
+					justifyContent={navSize === "large" ? "start" : "center"}
 				>
-					<BsGraphUpArrow size={28} />
+					<BsGraphUpArrow size={27} />
+					{navSize === "large" ? (
+						<Text color={"white"} ml={"12px"} mt={"2px"} fontSize={"16px"}>
+							Report
+						</Text>
+					) : null}
 				</Flex>
 				<Flex
 					mb={"20px"}
+					color={"white"}
 					transition="transform 0.3s ease-in-out"
 					_hover={{ transform: "scale(1.2)" }}
-					justifyContent={"center"}
+					ml={navSize === "large" ? "20px" : "0px"}
+					justifyContent={navSize === "large" ? "start" : "center"}
 				>
 					<BsPersonGear size={32} />
+					{navSize === "large" ? (
+						<Text color={"white"} ml={"12px"} mt={"2px"} fontSize={"16px"}>
+							Menu
+						</Text>
+					) : null}
 				</Flex>
 			</Box>
+			<Flex mb={"20px"} justifyContent={"center"} color={"white"}>
+				<SlLogout size={25} />
+				{navSize === "large" ? (
+						<Text color={"white"} ml={"11px"} mt={"2px"} fontSize={"16px"}>
+							Logout
+						</Text>
+					) : null}
+			</Flex>
 		</Flex>
 	);
 };
