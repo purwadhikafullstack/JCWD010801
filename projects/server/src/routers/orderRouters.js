9d8507d1831037fa5459b1122dc01b5df38aa251
rifkyrizkita <rifky.rizkita.g@gmail.com>
2023-09-19 10:53:42 +0700

    Update orderRouters.js

--- projects/server/src/routers/orderRouters.js
+++ projects/server/src/routers/orderRouters.js
@@ -3,8 +3,8 @@ const { orderControllers } = require("../controllers");
 const { verifyToken } = require("../middlewares/auth");
 
 router.get("/", verifyToken, orderControllers.ordersList);
-router.get("/", verifyToken, orderControllers.order);
+router.post("/", verifyToken, orderControllers.order);
 router.post("/shipment", orderControllers.shipment);
-router.patch("/",verifyToken, orderControllers.checkout);
+
 
 module.exports = router;
