04446f7bf81f93a76e26bc67afa70a530468132d
Raffael47 <132432134+Raffael47@users.noreply.github.com>
2023-09-25 10:15:28 +0700

    [OGWA-31] As a user, I want to confirm the delivery of my order. Landing page banner update.

--- projects/client/src/views/Home/banner/index.jsx
+++ projects/client/src/views/Home/banner/index.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import { EmblaCarousel } from './carousel'
-import './embla.css'
-import {images} from './imageByIndex'
+import { Navigation, Pagination, Autoplay, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import "./banner.css"
+import slide1 from "../../../assets/public/AM_logo.png";
+import slide2 from "../../../assets/public/AM_bg_login.png";
+import slide3 from "../../../assets/public/AM_backgroundLogin.png";
+import { Image } from '@chakra-ui/react';
 
-const OPTIONS = { loop: true }
-// const SLIDES = Array.from(Array(images.length).keys())
-const SLIDES = Array.from(Array(images.length).keys())
-
-export const Banner = ({ slides, options }) => {
-    return (
-        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
-    )
-}
+export const Banner = () => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, A11y, Autoplay]}
+      spaceBetween={50}
+      loop={true}
+      slidesPerView={1}
+      navigation
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false
+      }}
+      pagination={{ clickable: true }}
+    >
+      <SwiperSlide> <Image height={{ base: "200px", lg: "auto" }} src={slide1} w={'500px'} /> </SwiperSlide>
+      <SwiperSlide> <Image height={{ base: "200px", lg: "auto" }} src={slide2} w={'500px'} /> </SwiperSlide>
+      <SwiperSlide> <Image height={{ base: "200px", lg: "auto" }} src={slide3} w={'500px'} /> </SwiperSlide>
+    </Swiper>
+  );
+};
