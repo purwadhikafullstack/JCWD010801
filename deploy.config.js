b36e91dc3020016041617547401adbf8fd1f34c3
gangsararyop <86463063+gangsararyop@users.noreply.github.com>
2023-10-08 11:39:18 +0700

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "JCWD-2000-01", // Format JCWD-{batchcode}-{groupnumber}
+      name: "JCWD-0108-01", // Format JCWD-{batchcode}-{groupnumber}
       script: "./projects/server/src/index.js",
       env: {
         NODE_ENV: "production",
-        PORT: 8000,
+        PORT: 8801,
       },
       time: true,
     },
