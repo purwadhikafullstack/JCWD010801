aba86cbc2a184d6c0770a365c0be294703e403ed
Amadeo Marli <132928847+asc33nzio@users.noreply.github.com>
2023-10-14 23:27:33 +0700

    Deploy v.1.7 (#113)
    * Deploy v.1.7

--- projects/server/src/models/branches.js
+++ projects/server/src/models/branches.js
@@ -5,9 +5,9 @@ module.exports = (sequelize, DataTypes) => {
 		static associate(models) {
 			Branches.hasMany(models.Users);
 			Branches.hasMany(models.StockMovements);
-			Branches.belongsToMany(models.Products, {
-				through: models.Stocks,
-			});
+			// Branches.belongsToMany(models.Products, {
+			// 	through: models.Stocks,
+			// });
 			Branches.hasMany(models.Discounts);
 			Branches.hasMany(models.Vouchers);
 		}
