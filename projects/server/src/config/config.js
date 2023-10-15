85e86588c7d12bf0bdcd39f3fc50e6198cd3165a
asc33nzio <asc33nzio.dev@gmail.com>
2023-10-15 16:03:57 +0700

    [OGWA-38] Merge sync with dev@latest

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 
 module.exports = {
 	development: {
-		username: "jcwd010801",
-		password: "jcwd010801",
-		database: "jcwd010801",
-		host: "adminer2.purwadhikabootcamp.com",
+		username: process.env.USERNAME_DATABASE,
+		password: process.env.PASSWORD_DATABASE,
+		database: process.env.NAME_DATABASE,
+		host: "127.0.0.1",
 		dialect: "mysql",
 	},
 	test: {
-		username: "jcwd010801",
-		password: "jcwd010801",
-		database: "jcwd010801",
-		host: "adminer2.purwadhikabootcamp.com",
+		username: process.env.USERNAME_DATABASE,
+		password: process.env.PASSWORD_DATABASE,
+		database: process.env.NAME_DATABASE,
+		host: "127.0.0.1",
 		dialect: "mysql",
 	},
 	production: {
