67a145e891e78d3b66f9c9d51e9222741e37ffed
Amadeo Marli <132928847+asc33nzio@users.noreply.github.com>
2023-10-15 14:59:08 +0700

    Deploy V.2.3

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 
 module.exports = {
 	development: {
-		username: process.env.USERNAME_DATABASE,
-		password: process.env.PASSWORD_DATABASE,
-		database: process.env.NAME_DATABASE,
-		host: "127.0.0.1",
+		username: "jcwd010801",
+		password: "jcwd010801",
+		database: "jcwd010801",
+		host: "adminer2.purwadhikabootcamp.com",
 		dialect: "mysql",
 	},
 	test: {
-		username: process.env.USERNAME_DATABASE,
-		password: process.env.PASSWORD_DATABASE,
-		database: process.env.NAME_DATABASE,
-		host: "127.0.0.1",
+		username: "jcwd010801",
+		password: "jcwd010801",
+		database: "jcwd010801",
+		host: "adminer2.purwadhikabootcamp.com",
 		dialect: "mysql",
 	},
 	production: {
