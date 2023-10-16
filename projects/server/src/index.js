20a007bdd8f0cfb50f99563e92803005935b3fa0
Amadeo Marli <132928847+asc33nzio@users.noreply.github.com>
2023-10-16 13:26:23 +0700

    Deploy v.4.7 (API Adjustment) (#180)
    * Deploy v.4.6 (Merge Sync) (#177)
    
    Deploy v.4.6 (Merge Sync) (#177)
    
    * Deploy v.4.7 (API Adjustment) (#179)
    
    Deploy v.4.7 (API Adjustment)

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -1,4 +1,5 @@
-require("dotenv/config");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const express = require("express");
 const cors = require("cors");
 const { join } = require("path");
