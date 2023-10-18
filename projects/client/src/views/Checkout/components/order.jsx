b0229f5407704c129300416690dd73fa3ffa18bc
Amadeo Marli <132928847+asc33nzio@users.noreply.github.com>
2023-10-18 17:36:16 +0700

    Deploy v.5.6.a (Debug Production Cart Issues) (#220)
    # STAGING
    
    * Deploy v.3.7 (#147) -- sequelize update@latest v.6.33.0
    * Deploy v.4.1 (#160) Ready For Hosting On Linode
    * Deploy v.4.6 (**LIVE**-**LIVE**-**LIVE**)
    * Deploy v.4.8 (API Adjustment -- serve static) (#181) (#182)
    * Deploy v.5.0 SMTP & Handlebars Path Update + Hotfixes
    * Deploy v.5.1.a (Clean Prod Stable Build) (#189)
    
    # ALPHA RELEASE <sup>17/10/23</sup>
    ## V.5.1.a
    
    * Alpha Release V.5.1 <sup>17/10/23</sup>
    * Merge Sync v.5.1.a to OGWA-38: @asc33nzio 's base development branch
    
    # DEBUGGING
    * Deploy v.5.2.a (#197)
    * Deploy v.5.3.a (Debug API Endpoint)
    * Deploy v.5.3.1.a (Debug API Endpoint) (#200)
    * Deploy v.5.3.3.a (Debug API Endpoint -- Fixed Sequelize Literal Capitalization)
    * Deploy v.5.3.4.a (--__idem__--)
    * Deploy v.5.4.a (--__idem__--)
    * Deploy v.5.5.1.a (Debug Production Cart Issues)
    
    # PRODUCTION TEST
    * Deploy v.5.5.a (Post Sequelize Hotfixes--Pre Beta Stability Test)

--- projects/client/src/views/Checkout/components/order.jsx
+++ projects/client/src/views/Checkout/components/order.jsx
@@ -61,7 +61,7 @@ function Order() {
 					Authorization: `Bearer ${token}`,
 				},
 			});
-			if (response.data.cart_items.length === 0) {
+			if (response?.data?.cart_items?.length === 0) {
 				toast.warn("Cart is empty", {
 					position: "top-right",
 					autoClose: 5000,
@@ -100,7 +100,7 @@ function Order() {
 					Authorization: `Bearer ${token}`,
 				},
 			});
-			if (response.data.result.length === 0) {
+			if (response?.data?.result?.length === 0) {
 				navigate("/profile#addresses");
 				toast.warn("You must add your address first, before continue your order", {
 					position: "top-right",
